4bf8b66c727293542c5b845b418abd8edde82d15
I0HuKc <web-disain@mail.ua>
2021-12-04 11:48:17 +0300

    Серверная подготовка

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -11,11 +11,13 @@ import (
 )
 
 var (
-	configPath string
+	prod bool
+	cpu  int
 )
 
 func init() {
-	flag.StringVar(&configPath, "config-path", "config/config.local.toml", "Path to config file")
+	flag.BoolVar(&prod, "prod", false, "Strat on production server.")
+	flag.IntVar(&cpu, "cpu", 2, "Number of processor threads")
 }
 
 func main() {
@@ -23,13 +25,22 @@ func main() {
 	ctx := context.Background()
 
 	// Инициализирую конфигурацию
-	config := config.Init()
-	if _, err := toml.DecodeFile(configPath, config); err != nil {
-		panic(err)
+	var cnf *config.Config
+	if prod {
+		cnf = config.Init()
+		if _, err := toml.DecodeFile("config/config.prod.toml", cnf); err != nil {
+			panic(err)
+		}
+
+	} else {
+		cnf = config.Init()
+		if _, err := toml.DecodeFile("config/config.local.toml", cnf); err != nil {
+			panic(err)
+		}
 	}
 
 	// Инициализация модуля приложения
-	application := app.Init(config)
+	application := app.Init(cnf)
 	if err := application.Start(ctx); err != nil {
 		fmt.Println(err)
 	}
